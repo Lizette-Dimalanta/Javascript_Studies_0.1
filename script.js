e5caf454504151b5a54ed2660bd2f61a02f764a4
Lizette Dimalanta <105244120+Lizette-Dimalanta@users.noreply.github.com>
2022-12-01 23:45:43 +1100

    Initial Commit

---
+++ script.js
@@ -0,0 +1,42 @@
+// CALLBACKS
+
+// Asynchronous
+// function adder(x, y, callback) { // `callback/result` are placeholder names.
+//     setTimeout(() => callback(x + y), 3000) // <- Add delay (3 secs)
+//     console.log('Counter started')
+// }
+
+// adder(5, 10, result => console.log(result)) // callback
+// adder(15, 20, result => alert(result)) // callback
+
+// console.log('Hello')
+
+// Postman Equivalent (JS)
+
+// Configure Request
+function getJoke(cb) {
+    req = new XMLHttpRequest() // Does not have to be XML data
+    req.addEventListener('load', event => cb(event.target.response.joke))
+    req.open('GET', 'https://icanhazdadjoke.com/') // opens connection to server
+    req.setRequestHeader('Accept', 'application/json')
+    req.responseType = 'json'
+    req.send() // Sends request (Asynchronous)
+}
+
+// Websocket -> VS Live Server
+
+// getJoke(joke => console.log(joke))
+// getJoke(joke => document.body.innerHTML += `<p>${joke}</p>`) // Places joke into HTML
+
+// console.log('Waiting for joke...')
+
+const jokes = []
+
+getJoke(joke => { // callback
+    jokes.push(joke)
+    console.log(jokes)
+    getJoke(joke => { // callback
+        jokes.push(joke)
+        console.log(jokes)
+    })
+})
